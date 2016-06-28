130bcb67b1133416d4350628fed3546c054d37d5
xiz-tky <shizuka.kakimoto@linkage.jp.net>
2016-06-29 02:12:38 +0900

    add some elementals

---
+++ tcp_client.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import socket
+
+target_host = "0.0.0.0"
+target_port = 9999
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host, target_port))
+
+client.send("GET / HTTP/1.1\r\nHOST: google.com\r\n\r\n")
+
+response = client.recv(4096)
+
+print response
